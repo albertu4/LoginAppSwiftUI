b7621453cdba967b369c25aa7a52dd38d1fef716
Miwka <97275239+albertu4@users.noreply.github.com>
2022-05-24 17:37:55 +0900

    Initial Commit

---
+++ LoginAppSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LoginAppSwiftUI
+//
+//  Created by Михаил Иванов on 24.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LoginAppSwiftUI/LoginAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppSwiftUIApp.swift
+//  LoginAppSwiftUI
+//
+//  Created by Михаил Иванов on 24.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
